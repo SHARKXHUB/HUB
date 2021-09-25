b2e0c57301c52908bc1ccd380114b868f7c3428a
SHARKXHUB <85672631+SHARKXHUB@users.noreply.github.com>
2021-09-25 23:02:13 +0700

    Update Whitelist.php

--- Whitelist.php
+++ Whitelist.php
@@ -1,12 +1,4 @@
 <?php
-$keys = array('test1', "test2",'test3');
-$sub = $_GET["key"]; 
-$no = "haha";
-if (in_array($sub,$keys,TRUE)) {
-echo ("Whitelisted");
-}
-else
-{
-Echo ("not whitelisted");
-}
+date_default_timezone_set('GMT');
+echo date('h:i');
 ?>
